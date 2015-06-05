723ab94f5b4e5d09f559de8037fb6a667d47bcc6
Luc Jourdain <jourdain-luc@neuf.fr>
2015-06-05 11:06:30 +0200

    Création de la classe OutilsBDD

---
+++ src/main/java/mainpack/OutilsBDD.java
@@ -0,0 +1,24 @@
+package mainpack;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class OutilsBDD {
+	private Connection con;
+	private Statement stmt;
+
+	public OutilsBDD() {
+
+	}
+
+	private void connect() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:database.db");
+			stmt = (Statement) con.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
