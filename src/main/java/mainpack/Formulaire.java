c213b4c58b349cf45234b3e25b5701097e93066a
JLA23 <eric.lefebvre59@orange.fr>
2015-06-05 11:12:01 +0200

    Formulaire

---
+++ src/main/java/mainpack/Formulaire.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/servlet/Formulaire")
+public class Formulaire extends HttpServlet{
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		PrintWriter out = res.getWriter();
+		res.setContentType("text/html");
+		//out.println("<html><head><meta charset=UTF-8>");
+		out.println("<link rel=stylesheet type=text/css href=style.css>");
+		out.println("<title>Formulaire</title></head>");
+		out.println("<body><center>");
+		out.println("<table cellpadding=10>");
+		out.print("<FORM METHOD='POST' ACTION='VerifForm'>");
+		out.println("<TR><TD>Nom (*) : <TD><INPUT type='text' value='' name='nom'></TD></TR>");
+		out.println("<TR><TD>Prénom (*) :<TD> <INPUT type='text' value='' name='prenom'></TD></TR>");
+		out.println("<TR><TD>Entreprise (*) : <TD><INPUT type='text' value='' name='entreprise'></TD></TR>");
+		out.println("<TR><TD>Adresse (*) : <TD><INPUT type='text' value='' name='adresse'></TD></TR>");
+		out.println("<TR><TD>Téléphone (*) : <TD><INPUT type='text' value='' name='tel'></TD></TR>");
+		out.println("<TR><TD>E-mail (*) : <TD><INPUT type='email' value='' name='mail'></TD></TR>");
+		out.println("<TR><TD>Fax : <TD><INPUT type='text' value='' name='nom'></TD></TR>");
+		out.println("<TR><TD>Profession (*) : <TD><INPUT type='text' value='' name='profession'></TD></TR>");
+		out.println("<TR><TD>Description : <TD><INPUT type='text' value='' name='description'></TD></TR>");
+		out.println("<TR><TD>Facebook : <TD><INPUT type='url' value='' name='facebook'></TD></TR>");
+		out.println("<TR><TD>Twitter : <TD><INPUT type='url' value='' name='twitter'></TD></TR>");
+		out.println("<TR><TD>LinkedIn : <TD><INPUT type='url' value='' name='linkedin'></TD></TR>");
+
+		out.println("<TR><TD><TD><INPUT type='submit' value='Valider'></FORM></TD></TR></table></center></body><br><br>");
+	}
+	
+
+}
