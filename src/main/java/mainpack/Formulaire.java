f7fc70e4c642047b2284896a9e4bc209ac5305e0
Clement SORANO <clement.sorano@etudiant.univ-lille1.fr>
2015-06-05 16:49:03 +0200

    Mise à jour Formulaire

--- src/main/java/mainpack/Formulaire.java
+++ src/main/java/mainpack/Formulaire.java
@@ -1,41 +1,59 @@
 package mainpack;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.ProcessBuilder.Redirect;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("Formulaire")
 public class Formulaire extends HttpServlet{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		boolean mdpEgaux;
+		boolean mailEgaux;	
+		
 		PrintWriter out = res.getWriter();
 		res.setContentType("text/html");
-		int error = Integer.parseInt(req.getParameter("error"));
-		//out.println("<html><head><meta charset=UTF-8>");
+		
+		out.println("<html><head><meta charset=UTF-8>");
 		out.println("<link rel=stylesheet type=text/css href=style.css>");
 		out.println("<title>Formulaire</title></head>");
 		out.println("<body><center>");
 		out.println("<h1>Formulaire Inscription</h1>" );
-		if (error == 1){
-			out.println("Veuillez remplir les champs avec ( * )");
-		}
+		
 		out.println("<table cellpadding=10>");
-		out.print("<FORM METHOD='POST' ACTION='VerifForm'>");
-		out.println("<TR><TD>Nom (*) : <TD><INPUT type='text' value='' name='nom'></TD></TR>");
-		out.println("<TR><TD>Prénom (*) :<TD> <INPUT type='text' value='' name='prenom'></TD></TR>");
-		out.println("<TR><TD>Entreprise (*) : <TD><INPUT type='text' value='' name='entreprise'></TD></TR>");
-		out.println("<TR><TD>Adresse (*) : <TD><INPUT type='text' value='' name='adresse'></TD></TR>");
-		out.println("<TR><TD>Téléphone (*) : <TD><INPUT type='text' value='' name='tel'></TD></TR>");
-		out.println("<TR><TD>E-mail (*) : <TD><INPUT type='text' value='' name='mail'></TD></TR>");
-		out.println("<TR><TD>Fax : <TD><INPUT type='text' value='' name='fax'></TD></TR>");
-		out.println("<TR><TD>Profession (*) : <TD><INPUT type='text' value='' name='profession'></TD></TR>");
-		out.println("<TR><TD>Description : <TD><INPUT type='text' value='' name='description'></TD></TR>");
-		out.println("<TR><TD>Facebook : <TD><INPUT type='text' value='' name='facebook'></TD></TR>");
-		out.println("<TR><TD>Twitter : <TD><INPUT type='text' value='' name='twitter'></TD></TR>");
-		out.println("<TR><TD>LinkedIn : <TD><INPUT type='text' value='' name='linkedin'></TD></TR>");
+		out.print("<form method='post' action='VerifForm'>");
+		out.println("<tr><td>Nom (*) : <td><INPUT type='text' value='' name='nom' required></td>");
+		out.println("<td>Prénom (*) :<td> <INPUT type='text' value='' name='prenom' required></td>");
+		out.println("<tr><td>Entreprise (*) : <TD><INPUT type='text' value='' name='entreprise' required></td>");
+		out.println("<td>Adresse (*) : <td><INPUT type='text' value='' name='adresse' required></td>");
+		out.println("<tr><td>Code Postal (*) : <td><INPUT type='text' value='' name='codepost' required></td>");
+		out.println("<td>Ville (*) : <td><INPUT type='text' value='' name='ville' required></td>");
+		out.println("<tr><td>E-mail (*) : <td><INPUT type='email' value='' name='mail' required placeholder='thierry.dupont@gmail.com'></td>");
+		out.println("<td>Confirmer E-mail (*) : <td><INPUT type='email' value='' name='mail2' required placeholder='thierry.dupont@gmail.com'></td>");
+		out.println("<tr><td>Téléphone (*) : <td><INPUT type='tel' value='' name='tel' required placeholder='0675000000'></td>");
+		
+		out.println("<td>Fax : <td><INPUT type='text' value='' name='fax'></td>");
+		out.println("<tr><td>Profession (*) : <td><INPUT type='text' value='' name='profession' required></td>");
+		out.println("<td>Description : <td><INPUT type='text' value='' name='description'></td>");
+		out.println("<tr><td>Facebook : <td><INPUT type='text' value='' name='facebook' placeholder='http://monfacebook.fr/'/></td>");
+		out.println("<td>Twitter : <td><INPUT type='text' value='' name='twitter' placeholder='http://montwitter.fr/'></td>");
+		out.println("<tr><td>LinkedIn : <td><INPUT type='text' value='' name='linkedin' placeholder='http://monlinkedin.fr/'></td>");
+		out.println("<td>Google + : <td><INPUT type='text' value='' name='google+' placeholder='http://mongoogleplus.fr/'></td>");
+		out.println("<tr><td>Mot de passe (*) :<td><INPUT type='password' value='' name='password' required></td>");
+		out.println("<td>Confirmer Mot de passe (*) :<td><INPUT type='password' value='' name='password2' required></td>");
 
-		out.println("<TR><TD><TD><INPUT type='submit' value='Valider'></FORM></TD></TR></table></center></body><br><br>");
+		out.println("<tr><td>(*) : Champs requis<td><INPUT type='submit' value='Valider'></FORM></TD></TR></table></center></body><br><br>");
 	}
 	
 
