ee1eea1f04ac574e7a763cf1c449ada18ae3ec97
Luc Jourdain <jourdain-luc@neuf.fr>
2015-06-05 09:30:07 +0200

    création classe BDD

--- src/main/java/mainpack/BDD.java
+++ src/main/java/mainpack/BDD.java
@@ -2,4 +2,17 @@ package mainpack;
 
 public class BDD {
 
+	private Client[] tab;
+	
+	public BDD() {
+		tab = new Client[4];
+		tab[0] = new Client(1, "Pierre", "Dupont", "Celio");
+		tab[1] = new Client(2, "Jean", "Petit", "Kiabi");
+		tab[2] = new Client(3, "Sylvie", "Defives", "Nocibé");
+		tab[3] = new Client(4, "Roger", "Gros", "Boucherie Gros");
+	}
+	
+	public Client[] getClient() {
+		return this.tab;
+	}
 }
