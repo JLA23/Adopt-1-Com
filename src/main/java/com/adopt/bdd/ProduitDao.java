470e8fc8dfb91cdd8ac1a6943d037f3d9c937dab
Fabian AVELIN <fabian.avelin@etudiant.univ-lille1.fr>
2015-06-08 10:35:44 +0200

    avancé de la bdd

--- src/main/java/com/adopt/bdd/ProduitDao.java
+++ src/main/java/com/adopt/bdd/ProduitDao.java
@@ -1,25 +1,28 @@
 package com.adopt.bdd;
 
-import org.skife.jdbi.v2.sqlobject.Bind;
-import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
-import org.skife.jdbi.v2.sqlobject.SqlQuery;
-import org.skife.jdbi.v2.sqlobject.SqlUpdate;
-import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
-import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+import mainpack.Items.Produit;
 
-import fr.iutinfo.User;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 public interface ProduitDao {
-	@SqlUpdate("create table produits (idt integer primary key autoincrement, nom varchar(100), prenom varchar(100), entite varchar(100))")
+	@SqlUpdate("create table produits (idt integer primary key autoincrement, libelle varchar(100), photo varchar(255), description varchar(240), idVendeur integer, promo varchar(100), offreGroupe boolean, categorie varchar(100))")
 	void createClientTable();
 
-	@SqlUpdate("insert into produits (nom, prenom, entite) values (:nom, :prenom, :entite)")
+	@SqlUpdate("insert into produits (libelle, photo, description, idVendeur, promo, offreGroupe, categorie) values (:libelle, :photo, :description, :idVendeur, :promo, :offreGroupe, :categorie)")
 	@GetGeneratedKeys
-	int insert(@Bind("nom") String nom, @Bind("prenom") String prenom, @Bind("entite") String entite);
-
+	int insert(@BindBean Produit p);
+	
+	@SqlUpdate("update produits set libelle = :libelle, photo = :photo, description = :description, idVendeur = :idVendeur, promo = :promo, offreGroupe = :offreGroupe, categorie = :categorie")
+	int update(@BindBean Produit p);
+	
+	@SqlUpdate("delete from produits where idt = :idt")
+	int deleteProd(@Bind("idt")int idt);
 	
 	@SqlUpdate("drop table if exists produits")
-	void dropUserTable(); 
+	void dropProductTable(); 
 	
 	void close();
 }
--- src/main/java/mainpack/Items/Produit.java
+++ src/main/java/mainpack/Items/Produit.java
@@ -3,14 +3,26 @@ package mainpack.Items;
 public class Produit extends Item {
 
 	private String libelle;
+	private String photo;
 	private String prix;
 	private String description;
+	private int idVendeur;
+	private String promo;
+	private boolean offreGroupe;
+	private String catégorie;
 
-	public Produit(String id, String libelle, String prix, String description) {
+	public Produit(int id, String libelle, String photo, String prix, String description, int idVendeur, String promo, boolean offreGroupe, String catégorie) {
 		super(id);
-		this.libelle = libelle;
-		this.prix = prix;
-		this.description = description;
+		this.setLibelle(libelle);
+		this.setPrix(prix);
+		this.setDescription(description);
+		this.setPhoto(photo);
+		this.setIdVendeur(idVendeur);
+		this.setPromo(promo);
+		this.setOffreGroupe(offreGroupe);
+		this.setCatégorie(catégorie);
+		
+		
 	}
 	
 	public String getId() {
@@ -51,10 +63,50 @@ public class Produit extends Item {
 	}
 
 	public String renderHTML() {
-		String res = "<td>Id : " + id + "<br> Libellé : " + libelle
+		String res = "<td>Libellé : " + libelle
 				+ "<br> Prix : " + prix + "<br> Description : " + description;
 
 		return res;
+	}
+
+	public int getIdVendeur() {
+		return idVendeur;
+	}
+
+	public void setIdVendeur(int idVendeur2) {
+		this.idVendeur = idVendeur2;
+	}
+
+	public String getPromo() {
+		return promo;
+	}
+
+	public void setPromo(String promo) {
+		this.promo = promo;
+	}
+
+	public boolean getOffreGroupe() {
+		return offreGroupe;
+	}
+
+	public void setOffreGroupe(boolean offreGroupe) {
+		this.offreGroupe = offreGroupe;
+	}
+
+	public String getCatégorie() {
+		return catégorie;
+	}
+
+	public void setCatégorie(String catégorie) {
+		this.catégorie = catégorie;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
 	}
 
 }
