dac6a2b78066b3c249a96921c7091c1f5ccfa359
Theo ANGE <theo.ange@etudiant.univ-lille1.fr>
2015-06-09 10:42:24 +0200

    ProduitDao corrigé

--- src/main/java/com/adopt/bdd/ProduitDao.java
+++ src/main/java/com/adopt/bdd/ProduitDao.java
@@ -13,14 +13,14 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 
 public interface ProduitDao {
-	@SqlUpdate("create table produits (idt integer primary key autoincrement, libelle varchar(100), photo varchar(255), description varchar(240), idVendeur integer, promo varchar(100), offreGroupe boolean, categorie varchar(100))")
+	@SqlUpdate("create table produits (idt integer primary key autoincrement, libelle varchar(100), photo varchar(255), prix varchar(100), description varchar(240), idVendeur integer, promo varchar(100), offreGroupe boolean, categorie varchar(100))")
 	void createProductTable();
 
-	@SqlUpdate("insert into produits (libelle, photo, description, idVendeur, promo, offreGroupe, categorie) values (:libelle, :photo, :description, :idVendeur, :promo, :offreGroupe, :categorie)")
+	@SqlUpdate("insert into produits (libelle, photo, prix, description, idVendeur, promo, offreGroupe, categorie) values (:libelle, :photo, :prix, :description, :idVendeur, :promo, :offreGroupe, :categorie)")
 	@GetGeneratedKeys
 	int insert(@BindBean Produit p);
 	
-	@SqlUpdate("update produits set libelle = :libelle, photo = :photo, description = :description, idVendeur = :idVendeur, promo = :promo, offreGroupe = :offreGroupe, categorie = :categorie")
+	@SqlUpdate("update produits set libelle = :libelle, photo = :photo, prix = :prix, description = :description, idVendeur = :idVendeur, promo = :promo, offreGroupe = :offreGroupe, categorie = :categorie")
 	void update(@BindBean Produit p);
 	
 	@SqlQuery("select count(*) from produits")
