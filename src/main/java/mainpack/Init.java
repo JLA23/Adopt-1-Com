f7fe6de68fdda5ed457abee1224bb5fbbafc3d75
Theo ANGE <theo.ange@etudiant.univ-lille1.fr>
2015-06-09 11:11:08 +0200

    plus de bourbie

--- src/main/java/mainpack/Init.java
+++ src/main/java/mainpack/Init.java
@@ -130,6 +130,9 @@ public class Init {
 	
 	public void exempleClient(){
 		clientDao.insert(new Client(-1, null, "Bourbie", "Rito", "Bourbie & co", "18 rue du pingouin", "59000", "Lille", "bourbie@gmail.com", "bourbue", "01/01/2015", "0606060606", "0404040404", "Bourbiste", "Bourbe", "Blblblblblb", "http://facebourbe/bourbie", "http://twitter/bourbie", null, null, "service", "talent", true));
+		clientDao.insert(new Client(-1, null, "Bourbia", "Rita", "Bourbie & co", "18 rue du pingouin", "59000", "Lille", "bourbie@gmail.com", "bourbue", "01/01/2015", "0606060606", "0404040404", "Bourbiste", "Bourbe", "Blblblblblb", "http://facebourbe/bourbie", "http://twitter/bourbie", null, null, "service", "talent", true));
+		clientDao.insert(new Client(-1, null, "Bourbiu", "Ritu", "Bourbie & co", "18 rue du pingouin", "59000", "Lille", "bourbie@gmail.com", "bourbue", "01/01/2015", "0606060606", "0404040404", "Bourbiste", "Bourbe", "Blblblblblb", "http://facebourbe/bourbie", "http://twitter/bourbie", null, null, "service", "talent", true));
+		clientDao.insert(new Client(-1, null, "Bourbiy", "Rity", "Bourbie & co", "18 rue du pingouin", "59000", "Lille", "bourbie@gmail.com", "bourbue", "01/01/2015", "0606060606", "0404040404", "Bourbiste", "Bourbe", "Blblblblblb", "http://facebourbe/bourbie", "http://twitter/bourbie", null, null, "service", "talent", true));
 	}
 	
 	public void exempleProduit(){
