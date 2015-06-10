5b0112f1a2cbd0306a01835591eaa58768d8da14
Clement SORANO <clement.sorano@etudiant.univ-lille1.fr>
2015-06-10 10:56:30 +0200

    Poulet

--- src/main/java/mainpack/Formulaire2.java
+++ src/main/java/mainpack/Formulaire2.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
