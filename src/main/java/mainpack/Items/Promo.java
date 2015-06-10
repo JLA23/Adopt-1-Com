3a440c204091164f339afba196c9ee05a736809d
Theo ANGE <theo.ange@etudiant.univ-lille1.fr>
2015-06-10 08:39:07 +0200

    ??

--- src/main/java/mainpack/Items/Promo.java
+++ src/main/java/mainpack/Items/Promo.java
@@ -1,6 +1,5 @@
 package mainpack.Items;
 
-
 /**
  * 
  * @author anget La classe Promo permet de créer des Promos ainsi que leur
@@ -9,15 +8,14 @@ package mainpack.Items;
 
 public class Promo extends Item {
 
-
 	private int idProduit;
 	private int idService;
 	private String datefin;
 	private String remise;
 	private String description;
 
-	
-	public Promo(int id, int idProduit, int idService, String datefin, String remise, String description){
+	public Promo(int id, int idProduit, int idService, String datefin,
+			String remise, String description) {
 		super(id);
 		this.setIdProduit(idProduit);
 		this.setIdService(idService);
@@ -25,72 +23,65 @@ public class Promo extends Item {
 		this.setRemise(remise);
 		this.setDescription(description);
 	}
-		
-		
-	public String renderHTML() {
-	/*TODO	String res="<td>Id : "+id+"<br> Prix : "+prix+"<br> Prix de base : "+prixBase+"<br> Économisez "+reduc+"<br> Description : "+description+"<br> Date de fin : "+datefin;
 
-		return res;*/
+	public String renderHTML() {
+		/*
+		 * TODO String
+		 * res="<td>Id : "+id+"<br> Prix : "+prix+"<br> Prix de base : "
+		 * +prixBase+"<br> Économisez "+reduc+"<br> Description : "+description+
+		 * "<br> Date de fin : "+datefin;
+		 * 
+		 * return res;
+		 */
 		return null;
 	}
-
 
 	public int getIdProduit() {
 		return idProduit;
 	}
 
-
 	public void setIdProduit(int idProduit) {
 		this.idProduit = idProduit;
 	}
-
 
 	public String getDatefin() {
 		return datefin;
 	}
 
-
 	public void setDatefin(String datefin) {
 		this.datefin = datefin;
 	}
-
 
 	public String getRemise() {
 		return remise;
 	}
 
-
 	public void setRemise(String remise) {
 		this.remise = remise;
 	}
-
 
 	public String getDescription() {
 		return description;
 	}
 
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 
 	public int getIdService() {
 		return idService;
 	}
 
-
 	public void setIdService(int idService) {
 		this.idService = idService;
 	}
 
-
 	@Override
 	public String getTitle() {
-		return null; //TODO getTitle Promo
+		return null;
 	}
-	
-	///Renvoie le nom de la table dans laquelle doit être stockée l'Item
+
+	// /Renvoie le nom de la table dans laquelle doit être stockée l'Item
 	@Override
 	public String getType() {
 		return "promos";
